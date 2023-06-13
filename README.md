231e7d649fff6d7da4552594e60580d2c688b925
Emily McBride <a02291674@usu.edu>
2023-06-13 17:02:16 -0600

    added pokemon api

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Project-1-UsingVue
 
+## Using the Dog API with Vue.js
+* make fetch requests to https://dog.ceo/api/breeds/image/random and 
+using the JSON response to get the image of the Dog
--- static/script.js
+++ static/script.js
@@ -5,12 +5,20 @@ const app = Vue.createApp({
             error: false,
             dog: undefined,
             message: undefined,
+            pokemon: undefined,
+            name: undefined,
+            sprite: undefined,
         };
     },
     created() {
         var url1 = 'https://dog.ceo/api/breeds/image/random'
+        var randomPokemon = Math.floor(Math.random() * 1010)
+        var url2 = 'https://pokeapi.co/api/v2/pokemon'
         let myURL1 = `${url1}`
+        let myURL2 = `${url2}/${randomPokemon}`
         console.log(`The URL is ${myURL1}`)
+        console.log(`The URL2 is ${myURL2}`)
+
 
         fetch(myURL1)
         .then(r => r.json())
@@ -19,7 +27,17 @@ const app = Vue.createApp({
             console.log(`The image URL is: ${json.message}`)
             this.dog = json;
             this.message = json.message;
+            return fetch(myURL2)
 
+        })
+        .then(r => r.json())
+        .then(json => {
+            console.log(json);
+            this.pokemon = json;
+            this.name = json.name
+            console.log(json.name)
+            this.sprite = json.sprites.front_default
+            console.log(json.sprites.front_default)
 
         })
         .catch(err => this.error = true)
